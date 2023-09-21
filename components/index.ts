e2970dbd6c83a5c158255e1a47e61220fcf5af13
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-09-21 16:08:07 +0700

    release v1

---
+++ components/index.ts
@@ -0,0 +1,31 @@
+import Hero from './Hero';
+import CustomButton from "./CustomButton";
+import NavBar from './NavBar ';
+import Footer from './Footer';
+import CarCard from './CarCard';
+import ShowMore from './ShowMore';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+
+export { Hero, CustomButton, NavBar, Footer, CarCard, ShowMore, SearchBar, CustomFilter };
+
+
+// import CarCard from "./CarCard";
+// import CustomButton from "./CustomButton";
+// import CustomFilter from "./CustomFilter";
+// import Footer from "./Footer";
+// import NavBar from "./Navbar";
+// import ShowMore from "./ShowMore";
+// import SearchBar from "./Searchbar";
+// import Hero from "./Hero";
+
+// export {
+//   Hero,
+//   CarCard,
+//   CustomButton,
+//   CustomFilter,
+//   Footer,
+//   NavBar,
+//   ShowMore,
+//   SearchBar,
+// };
